9fe54773d47eaa1c075c625ffe15717877f99c80
Bushhar <86100372+Bushhar@users.noreply.github.com>
2023-06-12 18:24:28 +0700

    Update appa.py

--- appa.py
+++ appa.py
@@ -1,4 +1,6 @@
 import streamlit as st 
+import pandas as pd
+import numpy as np 
 from streamlit_option_menu import option_menu
 from streamlit_echarts import st_echarts
 import matplotlib.pyplot as plt
